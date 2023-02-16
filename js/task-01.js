100a2409bdab496178d3a4de4b8574d10aa0d295
Taras <81567782+BoikovTaras@users.noreply.github.com>
2023-02-16 23:19:24 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
 const list = document.querySelector('#categories');
 const item = list.querySelectorAll('.item')
 console.log(`У списку ${item.length} категорії`)
+item.forEach(
+    node => {
+        const title = node.querySelector('h2')
+        const list = node.querySelectorAll('li')
+        console.log(`В категорії ${title.textContent} знаходяться ${list.length} елементів `)
+    });
